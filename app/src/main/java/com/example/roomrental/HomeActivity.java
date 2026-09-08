8d7b628a9f30b55eac157f8dac367ad7424d7860
Ashok Poudel <poudelashok222@gmail.com>
2026-09-08 18:19:40 +0545

    added a home activity

--- app/src/main/java/com/example/roomrental/HomeActivity.java
+++ app/src/main/java/com/example/roomrental/HomeActivity.java
@@ -2,11 +2,30 @@ package com.example.roomrental;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigation);
+        bottomNav.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_home) {
+                return true;
+            } else if (itemId == R.id.nav_explore) {
+                return true;
+            } else if (itemId == R.id.nav_saved) {
+                return true;
+            } else if (itemId == R.id.nav_messages) {
+                return true;
+            } else if (itemId == R.id.nav_profile) {
+                return true;
+            }
+            return false;
+        });
     }
 }
