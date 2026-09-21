a59b5002a5035586b77c94ae904c1b31c6c221f7
Ashok Poudel <poudelashok222@gmail.com>
2026-09-21 17:49:34 +0545

    added  an exit button

--- app/src/main/java/com/example/roomrental/HomeActivity.java
+++ app/src/main/java/com/example/roomrental/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.roomrental;
+import android.content.DialogInterface;
 import android.widget.TextView;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.widget.Toast;
@@ -34,9 +37,37 @@ public class HomeActivity extends AppCompatActivity {
 
         textView.setText(name);
 
+        TextView exit = findViewById(R.id.tvSeeAll);
+        exit.setOnClickListener(v -> {
+            showDialog();
+        });
+
         Toast.makeText(HomeActivity.this,
                 "Login Successful ,Welcome " + name,
                 Toast.LENGTH_SHORT).show();
 
+
+
+
+    }
+
+    public void showDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Exit");
+        builder.setMessage("Do You Want To Close The Application?");
+        builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
